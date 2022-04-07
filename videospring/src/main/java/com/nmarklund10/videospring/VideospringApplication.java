ddaf28a3101f59137eebdab7183a117ba392b90a
Your Name <you@example.com>
2022-04-07 12:48:55 -0400

    add boilerplate code

---
+++ videospring/src/main/java/com/nmarklund10/videospring/VideospringApplication.java
@@ -0,0 +1,13 @@
+package com.nmarklund10.videospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideospringApplication.class, args);
+	}
+
+}
---
+++ videospring/src/test/java/com/nmarklund10/videospring/VideospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmarklund10.videospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
