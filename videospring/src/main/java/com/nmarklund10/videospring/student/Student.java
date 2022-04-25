5069b4ed26449d6d5e52252e76efedbec26bd31d
Your Name <you@example.com>
2022-04-25 15:03:02 -0400

    map student class to relation in database

--- videospring/src/main/java/com/nmarklund10/videospring/student/Student.java
+++ videospring/src/main/java/com/nmarklund10/videospring/student/Student.java
@@ -2,19 +2,29 @@ package com.nmarklund10.videospring.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private Integer age;
 
-    public Student(Long id,
-                   String name,
-                   String email,
-                   LocalDate dob,
-                   Integer age) {
-        this.id = id;
+    public Student(String name,
+            String email,
+            LocalDate dob,
+            Integer age) {
         this.name = name;
         this.email = email;
         this.dob = dob;
@@ -65,6 +75,5 @@ public class Student {
     public String toString() {
         return "Student [age=" + age + ", dob=" + dob + ", email=" + email + ", id=" + id + ", name=" + name + "]";
     }
-
 
 }
